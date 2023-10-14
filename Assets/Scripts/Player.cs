b885b84b27deaa62489431f0a0ae8087eb841191
KABOOTAR-1 <bt20cse052@iiitn.ac.in>
2023-10-14 15:35:12 +0530

    HitDetect

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Rendering.PostProcessing;
 public class Player:MonoBehaviour
 {
     int score=0;
     public static Player Instance;
+    public PostProcessVolume postProcessVolune;
     [SerializeField]
     TextMeshProUGUI Score;
+    Vignette vigentte;
     int health = 100;
+    float maxIntensity = 0.39f;
     private void Start()
     {
         if (Instance != this)
@@ -16,6 +20,8 @@ public class Player:MonoBehaviour
             Instance = this;
         }
         score = 0;
+        postProcessVolune.profile.TryGetSettings(out vigentte);
+        vigentte.intensity.value = 0;
     }
 
     public void IncreaseScore()
@@ -27,6 +33,7 @@ public class Player:MonoBehaviour
     public void DecreaseHealth()
     {
         health -= 10;
+        StartCoroutine(HitDetect());
         if (health <= 0)
         {
             Debug.Log("Player is dead");
@@ -41,5 +48,16 @@ public class Player:MonoBehaviour
     {
         string putUrl = ApiManager.MakeApiCall("update");
         StartCoroutine(ClientAPI.Put(putUrl, InputAndButtons.I_Username, score));
+    }
+
+    IEnumerator HitDetect()
+    {
+        while(vigentte.intensity.value<maxIntensity)
+        vigentte.intensity.value = Mathf.MoveTowards(vigentte.intensity.value, maxIntensity, Time.deltaTime*0.5f);
+        yield return new WaitForSeconds(5f);
+        while(vigentte.intensity.value>0)
+        vigentte.intensity.value = Mathf.MoveTowards(vigentte.intensity.value, 0, Time.deltaTime *0.5f);
+
+        yield return null;
     }
 }
